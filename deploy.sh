49b7cd567f5cfcabae3977ba45c91209cbc5da7c
Yuriy <yura.posledov@yandex.ru>
2024-04-30 18:06:27 +0300

    fix script for webhook service

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,11 @@
 git pull
 
 # Stop the existing Docker container named webhooks_server_container
-docker container stop webhooks_server_container
+docker container stop webhooks_server_container && docker container rm webhooks_server_container
 
-# Wait for a moment to ensure that Docker has stopped the container
-sleep 5
+docker rmi webhooks_server
 
-# Remove the existing Docker container named webhooks_server_container
-docker container rm webhooks_server_container
+docker build -t webhooks_server
 
 # Run a Docker container for the Webhooks_server application in detached mode
 docker run --detach -p 3000:3000 --name webhooks_server_container webhooks_server
