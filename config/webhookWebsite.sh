a6a2b46c319c2c8e4511c8f07f12d76abbd39726
Yuriy <yura.posledov@yandex.ru>
2024-06-07 13:09:42 +0300

    fix: remove unused commands

--- config/webhookWebsite.sh
+++ config/webhookWebsite.sh
@@ -6,14 +6,10 @@ cd ../RSL-website || return 1
 git pull
 
 # Stop and Remove the existing Docker container named strapi
-#(docker container stop website && docker container rm website) || echo "No resource found to remove"
 (docker container stop nginx-react-container && docker container rm nginx-react-container) || echo "No resource found to remove"
 
-#docker rmi myreactapp || echo "No resource found to remove"
 docker rmi nginx-react-image || echo "No resource found to remove"
 
-#docker build -t myreactapp .
 docker build -t nginx-react-image .
 
-#docker run --detach -p 3000:3000 --name website myreactapp
 docker run -d -p 80:80 --name nginx-react-container nginx-react-image
